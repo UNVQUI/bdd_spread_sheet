1d78efeab940e1aad9e8ddfd7f8045416d4652a0
rama <rama.croc@gmail.com>
2020-05-18 00:56:11 -0300

    42553 Modificacion Add Formula

--- src/test/java/gradle/cucumber/SpreadsheetStepdefs.java
+++ src/test/java/gradle/cucumber/SpreadsheetStepdefs.java
@@ -7,6 +7,9 @@ import cucumber.api.java.en.When;
 import sheet.Direccion;
 import sheet.SpreadSheet;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
@@ -26,6 +29,7 @@ public class SpreadsheetStepdefs {
         Integer numeroUno = Integer.parseInt(numero);
 
         sheet.set(dir, numeroUno);
+
     }
 
     @When("^I set \"([^\"]*)\" content with '([^\"]*)'$")
@@ -50,6 +54,23 @@ public class SpreadsheetStepdefs {
 
         Object expected = valor;
         assertEquals( expected, content);
+    }
+
+    @When("^I set \"([^\"]*)\" using a formula \"=suma([^\"]*)\"$")
+    public void i_set_using_a_formula(String direccion, String formula) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+        Direccion dir = new Direccion(direccion);
+        Integer numeroUno = 10;
+
+        Pattern parts = Pattern.compile("(\\w\\d),(\\w\\d)");
+        Matcher matcher = parts.matcher(formula);
+        matcher.find();
+        Integer result = (Integer)sheet.get(matcher.group(1)) + (Integer)sheet.get(matcher.group(2));
+        System.out.println(result);
+
+        sheet.set(dir, numeroUno);
+
     }
 
 }
